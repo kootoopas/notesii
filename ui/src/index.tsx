9f742017082a0ef9d45d41bcb0379287a2f14fb7
Fotis Koutoupas <fotis.koutoupas@gmail.com>
2019-11-03 01:28:00 +0200

    Add ui app bootstrap

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,5 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {NoteRepository} from './note/NoteRepository'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function initializeApp(noteRepository: NoteRepository) {
+  const NoteRepositoryContext = React.createContext<NoteRepository>(noteRepository)
+
+  ReactDOM.render(
+    <NoteRepositoryContext.Provider value={noteRepository}>
+      <App />
+    </NoteRepositoryContext.Provider>,
+    document.getElementById('root')
+  )
+}
+
+initializeApp(new NoteRepository('http://localhost:8000/api/v1/notes'))
